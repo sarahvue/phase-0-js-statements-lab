9b8e006dc801c3e4ea26f3b5415af74470f3965a
sarahvue <sarahvue0924@gmail.com>
2025-04-19 11:54:26 -0700

    Updated and solved by SV

--- gardenManagement.js
+++ gardenManagement.js
@@ -1,3 +1,21 @@
 const temperature = 90;
 const timeOfDay = "morning";
 let soilMoisture = 30;
+
+if (temperature > 80) {
+    console.log("Watering on");
+} else {
+    console.log("Watering off");
+}
+
+if (timeOfDay == "morning") {
+    console.log("Lights off");
+} else {
+    console.log("Lights on");
+}
+
+while (soilMoisture < 40) {
+    soilMoisture+=5;
+}
+console.log(soilMoisture);
+
